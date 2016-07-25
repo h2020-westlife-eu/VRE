652923fbde033806edc2a9c7ec93b5f05dc26295
Matthieu Riviere <mriviere@luna-technology.com>
2016-07-26 00:16:17 +0200

    Karma configuration

--- karma.conf.js
+++ karma.conf.js
@@ -106,12 +106,18 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['Chrome_without_sandbox', 'Firefox'],
 
+      customLaunchers: {
+        Chrome_without_sandbox: {
+            base: 'Chrome',
+                flags: ['--no-sandbox']
+        }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+    singleRun: false,
 
     // Concurrency level
     // how many browser should be started simultaneous

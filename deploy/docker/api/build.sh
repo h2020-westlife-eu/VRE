a2a6394eca0e1ebc7813d156603179b32ff697b6
Matthieu Riviere <mriviere@luna-technology.com>
2017-11-27 13:42:42 +0100

    Add xmlsec1 dependency

--- deploy/docker/api/build.sh
+++ deploy/docker/api/build.sh
@@ -5,6 +5,6 @@ set -eux
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update -y -qq
-apt-get install -y -qq build-essential python-dev postgresql-9.4
+apt-get install -y -qq build-essential python-dev postgresql-9.4 xmlsec1
 
 apt-get clean
